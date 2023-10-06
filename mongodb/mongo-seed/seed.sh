9987457e5c7c8d9d19fb859d98f0a16fc6d8d14e
Etienne Baumgartner <baume96@gmail.com>
2023-10-06 18:38:30 +0200

    swagger UI added

---
+++ mongodb/mongo-seed/seed.sh
@@ -0,0 +1,2 @@
+mongoimport --host mongodb --db codeSnippets --collection course --type json --file /codeSnippets.course.json --jsonArray
+mongoimport --host mongodb --db codeSnippets --collection snippets --type json --file /codeSnippets.snippets.json --jsonArray
